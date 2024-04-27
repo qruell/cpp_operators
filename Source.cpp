0a1c2b63c7a9d19e163237c6c51f06199c5b4ba9
qruell <illya.goncharuk.angelok@gmail.com>
2024-04-27 15:57:06 +0300

    ex2

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <Windows.h>
+using namespace std;
 
 int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
     int number1, number2;
     std::cout << "¬вед≥ть перше число: ";
     std::cin >> number1;
